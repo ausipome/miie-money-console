78f78c779a636a99f44bb1118da4536016d50a2d
Martyn Clinton <ausipome@gmail.com>
2024-07-16 07:39:50 +0100

    up

--- components/MiieLogin.tsx
+++ components/MiieLogin.tsx
@@ -1,51 +1,94 @@
 'use client'
 
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import useForm from '../hooks/useForm';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 export default function MiieLogin() {
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     const { values, handleChange } = useForm({
         email: '',
         password: '',
       });
     
-        const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            console.log(values); // You can use the form data here
-        };
+      // Handle login which returns a Json Web Token if successful
+      const handleLogin = async (e: any) => {
+        e.preventDefault();
+        try {
+          const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ values }),
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            router.push('/dashboard');
+          } else {
+            setError('Invalid email or password');
+          }
+        } catch (err) {
+          console.error(err);
+          setError('An error occurred. Please try again.');
+        }
+      };
 
         return (
 
             <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-slate-300">Login</h2>
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-                <div className="mb-4">
-               
-                <label htmlFor="email" className="block text-gray-600 text-sm font-semibold mb-2">Email:</label>
+            <form onSubmit={handleLogin} className="max-w-md mx-auto">
+                <div className="mb-4 text-center">
+
+                <div className='text-left'>
+                <label htmlFor="email" className="block text-gray-900 text-sm">Email</label>
                 <input
                     type="email"
                     id="email"
                     name="email"
+                    placeholder="Email"
                     value={values.email}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput"
                     required
                 />
 
-                <label htmlFor="password" className="block text-gray-600 text-sm font-semibold mb-2">Password:</label>
+                <label htmlFor="password" className="block text-gray-900 text-sm">Password</label>
+                <div className="password-container">
                 <input
-                    type="password"
+                    type={passwordVisible ? 'text' : 'password'}
                     id="password"
                     name="password"
+                    placeholder="Password"
                     value={values.password}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput password-input"
                     required
                 />
+                <span
+                className="toggle-password"
+                onClick={togglePasswordVisibility}
+                >
+                    <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+                </span>
+                </div>
+                </div>
 
-                <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-semibold p-2 rounded mt-4">Login</button>
-            <h3 className='text-center text-red-500'>Forgot Password</h3>
-            <h3 className='text-center text-pink-600'>Don't have an account? Sign up here!</h3>
+                <button type="submit" className="stdButton">Login</button>
+            <h3 className='text-center my-6'>Forgot your Password?</h3>
+            <h3 className='text-center my-6 text-[#5752FC]'><span className='text-slate-400'>Don't have an account? </span>Sign up here!</h3>
             </div>
             </form>
             </div>
--- components/MiieSignupForm.tsx
+++ components/MiieSignupForm.tsx
@@ -1,8 +1,17 @@
 'use client'
 
+import React, { useState } from 'react';
 import useForm from '../hooks/useForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export default function MiieSignupForm() {
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     const { values, handleChange } = useForm({
         firstName: '',
@@ -21,52 +30,80 @@ export default function MiieSignupForm() {
             <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded shadow-md">
             <h2 className="text-2xl font-semibold mb-4 text-slate-300">Sign Up</h2>
             <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-                <div className="mb-4">
-                <label htmlFor="firstName" className="block text-gray-600 text-sm font-semibold mb-2">First Name:</label>
+            <div className="mb-4 text-center">
+
+                <div className='text-left'>
+
+                <label htmlFor="firstName" className="block text-gray-900 text-sm">First Name</label>
                 <input
                     type="text"
                     id="firstName"
                     name="firstName"
+                    placeholder='First Name'
                     value={values.firstName}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput"
                     required
                 />
 
-                <label htmlFor="surname" className="block text-gray-600 text-sm font-semibold mb-2">Surname:</label>
+                <label htmlFor="surname" className="block text-gray-900 text-sm">Surname</label>
                 <input
                     type="text"
                     id="surname"
                     name="surname"
+                    placeholder='Surname'
                     value={values.surname}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput"
                     required
                 />
 
-                <label htmlFor="email" className="block text-gray-600 text-sm font-semibold mb-2">Email:</label>
+                <label htmlFor="country" className="block text-gray-900 text-sm">Country</label>
+                <input
+                    type="text"
+                    id="country"
+                    name="country"
+                    value="United Kingdom"
+                    disabled
+                    className="stdInput"
+                    required
+                />
+
+                <label htmlFor="email" className="block text-gray-900 text-sm">Email</label>
                 <input
                     type="email"
                     id="email"
                     name="email"
+                    placeholder='Email'
                     value={values.email}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput"
                     required
                 />
 
-                <label htmlFor="password" className="block text-gray-600 text-sm font-semibold mb-2">Password:</label>
+                <label htmlFor="password" className="block text-gray-900 text-sm">Password</label>
+                <div className="password-container">
                 <input
-                    type="password"
+                    type={passwordVisible ? 'text' : 'password'}
                     id="password"
                     name="password"
+                    placeholder='Password'
                     value={values.password}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                    className="stdInput password-input"
                     required
                 />
+                <span
+                className="toggle-password"
+                onClick={togglePasswordVisibility}
+                >
+                    <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+                </span>
+                </div>
+                </div>
 
-                <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-semibold p-2 rounded mt-4">Sign Up</button>
+                <h3 className='text-center my-6 text-[#5752FC]'><span className='text-slate-400'>Already have an account? </span>Sign in here!</h3>
+                <button type="submit" className="stdButton">Sign Up</button>
             </div>
             </form>
             </div>
